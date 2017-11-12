15df5b0cd6603b7f2cb1607ecaf0a306a4a4647e
Todd Carlson <tcarlson@carlsonsw.com>
2017-11-11 19:32:47 -0500

    checkpoint 13: completed

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -14,9 +14,10 @@
        </tr>
      `);
  
-     song.element.on('click', event => {
-       player.playPause(song);
-     });
+      song.element.on('click', event => {
+        player.playPause(song);
+        $('button#play-pause').attr('playState', player.playState);
+      });
       
      $('#song-list').append(song.element);    
    });

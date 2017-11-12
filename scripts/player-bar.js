c47ed474a79dd10595d7e0914ef918e2e7a00aa1
Todd Carlson <tcarlson@carlsonsw.com>
2017-11-12 10:59:34 -0500

    assignment 14 completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -23,8 +23,15 @@
      if (player.playState !== 'playing') { return; }
      const currentTime = player.getTime();
      const duration = player.getDuration();
+     const remainingTime = duration - currentTime;     
      const percent = (currentTime / duration) * 100;
-     $('#time-control .current-time').text( currentTime );     
+     $('#time-control .current-time').text( currentTime );
+     $('#time-control .total-time').text( remainingTime );          
      $('#time-control input').val(percent);              
    }, 1000);
+
+   $('#volume-control input').on('input', function (event) {
+     player.setVolume(event.target.value);
+   });
+
  }
